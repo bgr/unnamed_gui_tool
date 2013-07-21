87d0ee286015c954f37885345c7a90261876a9cc
bgr-ubuntu-vm <bgr.gyk@gmail.com>
2013-07-21 16:49:40 -0700

    Record metaclass prepends parent keys and forbids setattr

--- my_project/util.py
+++ my_project/util.py
@@ -6,9 +6,53 @@ def duplicates(ls):
     return [el for el, n in collections.Counter(ls).items() if n > 1]
 
 
+def with_rest(ls):
+    """Generator - yields tuples (element, all_other_elements)"""
+    for i, el in enumerate(ls):
+        yield (el, ls[:i] + ls[i + 1:])
+
+
+def with_succeeding(ls):
+    """Generator - yields tuples (element, successor_elements)"""
+    for i, el in enumerate(ls):
+        yield (el, ls[i + 1:])
+
+
+def with_preceding(ls):
+    """Generator - yields tuples (element, predecessor_elements)"""
+    for i, el in enumerate(ls):
+        yield (el, ls[:i])
+
+
 class _RecordMeta(type):
-    def __setattr__(self, _, __):
+
+    def __new__(mcs, name, bases, dct):
+        if len(bases) != 1 or (name != 'Record'
+                               and not issubclass(bases[0], Record)):
+            raise TypeError("Can only inherit from Record or its subclass")
+        if dct.get('keys') is None:
+            raise TypeError("Must specify mandatory 'keys' class variable")
+        if not isinstance(dct['keys'], (tuple, list)):
+            raise TypeError("Class variable 'keys' must be tuple or list")
+
+        # prepend super's keys - subclass must have all keys its parent has:
+        # collect keys starting from Record through its subclasses including
+        # keys from class currently being built
+        parent_keys = ([Par.keys for Par in reversed(bases[0].mro()[:-2])]
+                       + [list(dct['keys'])])
+        flat_keys = [k for pkeys in parent_keys for k in pkeys]
+
+        # filter out duplicate keys, parents' keys are included
+        # before any child's duplicate keys
+        dct['keys'] = tuple(k for k, seen in with_preceding(flat_keys)
+                            if k not in seen)
+
+        return type.__new__(mcs, name, bases, dct)
+
+    def __setattr__(cls, _, __):  # mainly to prevent modifying Class.keys
         raise TypeError("Nope")
+
+#re.match("[_A-Za-z][_a-zA-Z0-9]*$",my_var)
 
 
 class Record(tuple):
@@ -20,9 +64,11 @@ class Record(tuple):
             raise TypeError('{0} takes {1} items, got {2}'.format(
                 cls.__name__, len(cls.keys), len(args) + len(keyvals)))
 
+        # map positional args to as many keys as possible
         partial_keyvals = dict(zip(cls.keys, args))
         for key in partial_keyvals.keys():
             if key in keyvals:
+                print keyvals
                 raise TypeError("Argument '{0}' already specified "
                                 "using positional arguments".format(key))
 
@@ -30,12 +76,11 @@ class Record(tuple):
             if key not in cls.keys:
                 raise TypeError("Invalid keyword argument '{0}'".format(key))
 
-        complete_keyvals = dict(partial_keyvals.items() + keyvals.items())
+        complete = dict(partial_keyvals.items() + keyvals.items())
 
-        # order of values matters
-        vals = tuple([complete_keyvals[key] for key in cls.keys])
+        vals = tuple([complete[key] for key in cls.keys])  # order matters
 
-        ret = tuple.__new__(cls, vals)
+        ret = super(Record, cls).__new__(cls, vals)
         return ret
 
     def __getattr__(self, key):
--- tests/test_Record.py
+++ tests/test_Record.py
@@ -46,6 +46,7 @@ class Test_base_class:
         assert 'Record is immutable' in err.value
 
 
+
 class Test_inheritance:
 
     def setup_class(self):
@@ -159,25 +160,25 @@ class Test_many_arguments:
 
     def setup_class(self):
         class MyFiveThings(Record):
-            keys = ('a', 'bc', 'defg', 'h', 'i')
+            keys = ('x', 'bc', 'defg', 'h', 'i')
         self.C = MyFiveThings
 
     @pytest.mark.parametrize('create', [
         lambda self: self.C(2, 3, 4, 5, 6),
-        lambda self: self.C(a=2, bc=3, defg=4, h=5, i=6),
+        lambda self: self.C(x=2, bc=3, defg=4, h=5, i=6),
         lambda self: self.C(2, bc=3, defg=4, h=5, i=6),
         lambda self: self.C(2, 3, defg=4, h=5, i=6),
         lambda self: self.C(2, 3, 4, h=5, i=6),
         lambda self: self.C(2, 3, 4, 5, i=6),
-        lambda self: self.C(**{'a': 2, 'bc': 3, 'defg': 4, 'h': 5, 'i': 6}),
+        lambda self: self.C(**{'x': 2, 'bc': 3, 'defg': 4, 'h': 5, 'i': 6}),
         lambda self: self.C(2, **{'bc': 3, 'defg': 4, 'h': 5, 'i': 6}),
         lambda self: self.C(2, 3, **{'defg': 4, 'h': 5, 'i': 6}),
         lambda self: self.C(2, 3, 4, **{'h': 5, 'i': 6}),
         lambda self: self.C(2, 3, 4, 5, **{'i': 6}),
-        lambda self: self.C(a=2, **{'bc': 3, 'defg': 4, 'h': 5, 'i': 6}),
-        lambda self: self.C(defg=4, **{'bc': 3, 'a': 2, 'h': 5, 'i': 6}),
-        lambda self: self.C(bc=3, h=5, **{'defg': 4, 'a': 2, 'i': 6}),
-        lambda self: self.C(a=2, bc=3, defg=4, **{'h': 5, 'i': 6}),
+        lambda self: self.C(x=2, **{'bc': 3, 'defg': 4, 'h': 5, 'i': 6}),
+        lambda self: self.C(defg=4, **{'bc': 3, 'x': 2, 'h': 5, 'i': 6}),
+        lambda self: self.C(bc=3, h=5, **{'defg': 4, 'x': 2, 'i': 6}),
+        lambda self: self.C(x=2, bc=3, defg=4, **{'h': 5, 'i': 6}),
         lambda self: self.C(2, 3, h=5, **{'defg': 4, 'i': 6}),
         lambda self: self.C(2, 3, i=6, h=5, **{'defg': 4}),
     ])
@@ -185,6 +186,204 @@ class Test_many_arguments:
         c = create(self)
         assert c == (2, 3, 4, 5, 6)
         assert c == self.C(2, 3, 4, 5, 6)
-        assert str(c) == 'MyFiveThings(a=2, bc=3, defg=4, h=5, i=6)'
+        assert str(c) == 'MyFiveThings(x=2, bc=3, defg=4, h=5, i=6)'
         assert c.defg == 4
         assert c[2] == 4
+
+
+
+class Test_duplicated_keys_in_subclass:
+
+    def setup_class(self):
+        class Base(Record):
+            keys = ('x', 'a', 'b')
+        self.Base = Base
+
+    def test_repeat_some(self):
+        class Ch(self.Base):
+            keys = ('a', 'c')
+        assert Ch.keys == ('x', 'a', 'b', 'c')
+
+        class Grch(Ch):
+            keys = ('a', 'c', 'd')
+        assert Grch.keys == ('x', 'a', 'b', 'c', 'd')
+
+    def test_repeat_all(self):
+        class Ch(self.Base):
+            keys = ('a', 'x', 'b')
+        assert Ch.keys == ('x', 'a', 'b')
+
+        class Grch(Ch):
+            keys = ('x', 'd', 'b', 'c', 'a')
+        assert Grch.keys == ('x', 'a', 'b', 'd', 'c')
+
+
+
+class Test_grandchildren:
+
+    def setup_class(self):
+        class Base(Record):
+            keys = ('p1', 'mm')
+
+
+        class Ch1(Base):
+            keys = ('c11', 'c12')
+
+        class Grch11(Ch1):
+            keys = ('a111', 'g112')
+
+        class Grch12(Ch1):
+            keys = ['b', 'g122']
+
+
+        class Ch2(Base):
+            keys = ('c21', 'c22', 'c23')
+
+        class Grch21(Ch2):
+            keys = ['g211']
+
+        class Grch22(Ch2):
+            keys = ('x', 'yz')
+
+        self.Base = Base
+        self.Ch1 = Ch1
+        self.Ch2 = Ch2
+        self.Grch11 = Grch11
+        self.Grch12 = Grch12
+        self.Grch21 = Grch21
+        self.Grch22 = Grch22
+
+
+    def test_child_keys_appended_to_parent_keys(self):
+        assert self.Base.keys == ('p1', 'mm')
+        assert self.Ch1.keys == ('p1', 'mm', 'c11', 'c12')
+        assert self.Ch2.keys == ('p1', 'mm', 'c21', 'c22', 'c23')
+        assert self.Grch11.keys == ('p1', 'mm', 'c11', 'c12', 'a111', 'g112')
+        assert self.Grch12.keys == ('p1', 'mm', 'c11', 'c12', 'b', 'g122')
+        assert self.Grch21.keys == ('p1', 'mm', 'c21', 'c22', 'c23', 'g211')
+        assert self.Grch22.keys == ('p1', 'mm', 'c21', 'c22', 'c23', 'x', 'yz')
+
+    def test_cannot_instantiate_without_parents_keys(self):
+        with pytest.raises(TypeError) as err:
+            self.Ch1(2, 3)
+        assert 'takes 4' in err.value.message
+
+        with pytest.raises(TypeError) as err:
+            self.Ch2(c21=4, c22=5, c23=6)
+        assert 'takes 5' in err.value.message
+
+        with pytest.raises(TypeError) as err:
+            self.Grch22(4, 5, 6, 7, 8)
+        assert 'takes 7' in err.value.message
+
+    def test_instantiate_requires_parents_keys(self):
+        self.Ch1(p1=2, mm=3, c11=4, c12=5)
+        self.Ch2(p1=2, mm=3, c21=4, c22=5, c23=6)
+        self.Grch22(p1=2, mm=3, c21=4, c22=5, c23=6, x=7, yz=8)
+
+    def test_order_of_keyword_arguments(self):
+        c = self.Ch1(c12=5, mm=3, p1=2, c11=4)
+        gc = self.Grch22(2, 3, yz=8, c21=4, c23=6, c22=5, x=7)
+        assert c  == (2, 3, 4, 5)
+        assert gc == (2, 3, 4, 5, 6, 7, 8)
+
+    def test_can_get_values_via_keys(self):
+        assert self.Ch1(p1=2, mm=3, c11=4, c12=5).p1 == 2
+        assert self.Ch1(p1=2, mm=3, c11=4, c12=5).c11 == 4
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8).p1 == 2
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8).c21 == 4
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8).c22 == 5
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8).x == 7
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8).yz == 8
+
+    def test_can_get_values_via_index(self):
+        assert self.Ch1(p1=2, mm=3, c11=4, c12=5)[0] == 2
+        assert self.Ch1(p1=2, mm=3, c11=4, c12=5)[2] == 4
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8)[0] == 2
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8)[2] == 4
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8)[3] == 5
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8)[5] == 7
+        assert self.Grch22(2, 3, 4, c22=5, c23=6, x=7, yz=8)[6] == 8
+
+    def test_isinstance(self):
+        assert isinstance(self.Base(2, 3), Record)
+        c = self.Ch1(5, 6, 7, 8)
+        assert isinstance(c, Record)
+        assert isinstance(c, self.Base)
+        gc = self.Grch11(3, 4, 5, 6, 7, 8)
+        assert isinstance(gc, Record)
+        assert isinstance(gc, self.Base)
+        assert isinstance(gc, self.Ch1)
+        assert not isinstance(gc, self.Ch2)
+        assert not isinstance(gc, self.Grch12)
+
+    def test_cannot_change_values_via_properties(self):
+        b = self.Base(2, 3)
+        with pytest.raises(TypeError):
+            b.p1 = 3
+        assert b == (2, 3)
+
+        c = self.Ch1(5, 6, 7, 8)
+        with pytest.raises(TypeError):
+            c.p1 = 3
+        with pytest.raises(TypeError):
+            c.c12 = 3
+        assert c == (5, 6, 7, 8)
+
+        gc = self.Grch11(3, 4, 5, 6, 7, 8)
+        with pytest.raises(TypeError):
+            gc.p1 = 3
+        with pytest.raises(TypeError):
+            gc.c12 = 3
+        with pytest.raises(TypeError):
+            gc.g112 = 3
+        assert gc == (3, 4, 5, 6, 7, 8)
+
+    def test_cannot_change_values_via_index(self):
+        b = self.Base(2, 3)
+        with pytest.raises(TypeError):
+            b[1] = 2
+        assert b == (2, 3)
+
+        c = self.Ch1(5, 6, 7, 8)
+        with pytest.raises(TypeError):
+            c[1] = 2
+        with pytest.raises(TypeError):
+            c[3] = 2
+        assert c == (5, 6, 7, 8)
+
+        gc = self.Grch11(3, 4, 5, 6, 7, 8)
+        with pytest.raises(TypeError):
+            gc[3] = 3
+        with pytest.raises(TypeError):
+            gc[5] = 3
+        assert gc == (3, 4, 5, 6, 7, 8)
+
+    def test_cannot_mess_with_keys(self):
+        with pytest.raises(TypeError):
+            self.Base.keys = ('a', 'b',)
+        with pytest.raises(TypeError):
+            self.Ch1.keys = ('a', 'b',)
+        with pytest.raises(TypeError):
+            self.Grch12.keys = ('a', 'b',)
+
+        b = self.Base(2, 3)
+        c = self.Ch1(5, 6, 7, 8)
+        gc = self.Grch11(3, 4, 5, 6, 7, 8)
+        with pytest.raises(TypeError):
+            b.keys = ('a', 'b',)
+        with pytest.raises(TypeError):
+            c.keys = ('a', 'b',)
+        with pytest.raises(TypeError):
+            gc.keys = ('a', 'b',)
+
+        assert b.keys == ('p1', 'mm')
+        assert c.keys == ('p1', 'mm', 'c11', 'c12')
+        assert gc.keys == ('p1', 'mm', 'c11', 'c12', 'a111', 'g112')
+        assert self.Base.keys == ('p1', 'mm')
+        assert self.Ch1.keys == ('p1', 'mm', 'c11', 'c12')
+        assert self.Ch2.keys == ('p1', 'mm', 'c21', 'c22', 'c23')
+        assert self.Grch11.keys == ('p1', 'mm', 'c11', 'c12', 'a111', 'g112')
+        assert self.Grch12.keys == ('p1', 'mm', 'c11', 'c12', 'b', 'g122')
+        assert self.Grch21.keys == ('p1', 'mm', 'c21', 'c22', 'c23', 'g211')
+        assert self.Grch22.keys == ('p1', 'mm', 'c21', 'c22', 'c23', 'x', 'yz')
